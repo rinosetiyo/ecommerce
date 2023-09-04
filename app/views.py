c2ad508f2d64bb3d9f2ac77036f353db204ef5b0
rino setiyo <rino.setiyo96@gmail.com>
2023-09-04 14:52:03 +0700

    modify index.html

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from app.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'pages/index.html')
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products':products,
+    }
+    return render(request, 'pages/index.html', context)
 
 def product_detail(request):
     return render(request, 'pages/product-detail.html')
