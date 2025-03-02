57357847bd39eb034fddc70a37637c709bea71c0
rinosetiyo <rino.setiyo96@gmail.com>
2025-03-02 23:13:09 +0700

    add initial files for tablib and import_export packages, and configure jazzmin in settings

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-party apps
+    'jazzmin',
+    'import_export',
 ]
 
 MIDDLEWARE = [
