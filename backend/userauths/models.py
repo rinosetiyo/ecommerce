5ddf74b52908d0769be78550333c1b71d0c57857
rinosetiyo <rino.setiyo96@gmail.com>
2025-03-10 00:24:55 +0700

    restructure file

---
+++ backend/userauths/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from shortuuid.django_fields import ShortUUIDField # type: ignore
+
+# Create your models here.
+class User(AbstractBaseUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    full_name = models.CharField(max_length=50, blank=True, null=True)
+    phone = models.CharField(max_length=30, blank=True, null=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+    
+    def save(self, *args, **kwargs):
+        email_username = self.email.split('@')
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = email_username
+        if self.username == "" or self.username == None:
+            self.username = email_username
+
+        super(User, self).save(*args, **kwargs)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    image = models.FileField(upload_to="profiles/", default="default/default-user.jpg" ,blank=True, null=True)
+    full_name = models.CharField(max_length=50, blank=True, null=True)
+    about = models.TextField(blank=True, null=True)
+    gender = models.CharField(max_length=20, blank=True, null=True)
+    country = models.CharField(max_length=50, blank=True, null=True)
+    state = models.CharField(max_length=50, blank=True, null=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+    pid = ShortUUIDField(unique=True, length=10, max_length=20, alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    def __str__(self):
+        if self.full_name:
+            return str(self.full_name)
+        else:
+            return str(self.user.full_name)
+        
+    def save(self, *args, **kwargs):
+        if self.full_name == "" or self.full_name == None:
+            self.full_name = self.user.full_name
+
+        super(Profile, self).save(*args, **kwargs)
+    
