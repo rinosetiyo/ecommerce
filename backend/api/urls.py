8c5df385724b6b2ae1d71c1d158d6951d16d0ed9
rinosetiyo <rino.setiyo96@gmail.com>
2025-03-18 03:30:37 +0700

    add JWT token refresh endpoint and update authentication utility functions

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from userauths import views as userauths_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 # from store import views as store_views
 
 urlpatterns = [
     path('user/token/', userauths_views.MyTokenObtainPairView.as_view()),
+    path('user/token/refresh/', TokenRefreshView.as_view()),
     path('user/register/', userauths_views.RegisterView.as_view()),
 ]
--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,22 +1,103 @@
-// wrong path: frontend/src/utils/auth/auth.js
+import { useAuthStore } from '../store/auth';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import Cookies from 'js-cookie';
 
-import { create } from 'zustand';
-import { mountStoreDevtools } from 'simple-zustand-devtools';
+export const login = async (username, password) => {
+  try {
+    const { data, status } = await axios.post('user/token/', {
+      username,
+      password
+    });
+    if (status === 200) {
+      setAuthUser(data.access, data.refresh);
+      // alert('Login successful');
+    }
+    return { data, status: null };
+  } catch (error) {
+    return { 
+      data: null, 
+      error: error.response.data?.detail || 'Something went wrong', 
+    };
+  }
+};
 
-const useAuthStore = create((set, get) => ({
-  allUserData: null,
-  loading: false,
-  user: () => ({
-    user_id: get().allUserData ? get().allUserData.user_id : null,
-    username: get().allUserData ? get().allUserData.username : null,
-  }),
-  setUser: (user) => set({ allUserData: user }),
-  setLoading: (loading) => set({ loading }),
-  setLoggedIn: () => get().allUserData !== null
-}));
-
-if(import.meta.env.MODE) {
-mountStoreDevtools('Store', useAuthStore);
+export const register = async (full_name, email, phone, password, password2) => {
+  try {
+    const { data } = await axios.post('user/register/', {
+      full_name,
+      email,
+      phone,
+      password,
+      password2
+    });
+    await login(email, password);
+    // alert('Registration successful');
+    return { data, status: null };
+  } catch (error) {
+    return { 
+      data: null, 
+      error: error.response.data?.detail || 'Something went wrong', 
+    };
+  }
 }
 
-export { useAuthStore };
+export const logout = () => {
+  Cookies.remove('access_token');
+  Cookies.remove('refresh_token');
+  useAuthStore.getState().setUser(null);
+  // alert('Logout successful');
+}
+
+export const setUser = async () => {
+  Cookies.set('access_token', access);
+  Cookies.set('refresh_token', refresh);
+
+  if(!accesssToken || !refreshToken) {
+    return;
+  }
+
+  if(isAccessTokenExpired(accessToken)) {
+    const response = await getRefreshToken(refreshToken);
+    setAuthUser(response.access, refreshToken);
+  } else {
+    setAuthUser(accessToken, refreshToken);
+  }
+}
+
+export const setAuthUser = (access_token, refresh_token) => {
+  Cookies.set('access_token', access_token, {
+    expires: 1,
+    secure: true
+  });
+  Cookies.set('refresh_token', refresh_token, {
+    expires: 2,
+    secure: true
+  });
+
+  const user = jwt_decode(access_token);
+
+  if(user) {
+    useAuthStore.getState().setUser(user);
+  }
+  useAuthStore.getState().setLoading(false);
+}
+
+export const getRefreshToken = async () => {
+  const refresh_token = Cookies.get('refresh_token');
+  const response = await axios.post('user/token/refresh/', {
+    refresh: refresh_token
+  });
+  return response.data;
+}
+
+export const isAccessTokenExpired = (access_token) => {
+  try {
+    const decoded = jwt_decode(access_token);
+    return decoded.exp < Date.now() / 1000;
+  }
+  catch (error) {
+    console.log(error);
+    return true
+  }
+}
