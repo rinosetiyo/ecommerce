6dc5684b31ad05d0e8042ff2ff0826e21aa39a02
rinosetiyo <rino.setiyo96@gmail.com>
2025-03-29 19:47:38 +0700

    feat: add Dashboard and Logout components; update routing in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Login from './views/Login'
 import Register from './views/Register'
+import Dashboard from './views/auth/Dashboard'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ frontend/src/views/auth/Dashboard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useAuthStore } from '../../store/auth'
+import { Link } from 'react-router-dom'
+
+function Dashboard() {
+    const [isLoggedIn, setIsLoggedIn] = useAuthStore(state => ([
+        state.isLoggedIn,
+        state.setIsLoggedIn
+    ]))
+
+    return (
+        <div>
+            {isLoggedIn ? (
+                <div>
+                    <h1>Dashboard</h1>
+                    <p>Welcome to the dashboard!</p>
+                    <Link to="/logout">Logout</Link>
+                </div>
+            ) : (
+                <div>
+                    <h1>Home page</h1>
+                    <h1>Please log in</h1>
+                    <Link to="/login">Login</Link>
+                </div>
+            )}
+            <h2>Dashboard</h2>
+            <p>This is the dashboard page.</p>
+        </div>
+    )
+}
+
+export default Dashboard
