e64537515b9162efc9fe4a4aa4b90a3916cfa124
rinosetiyo <rino.setiyo96@gmail.com>
2025-04-09 23:55:43 +0700

    refactor: remove excessive navigation links from Logout component

--- frontend/src/views/auth/Logout.jsx
+++ frontend/src/views/auth/Logout.jsx
@@ -12,17 +12,6 @@ function Logout() {
             <h1>Logout</h1>
             <Link to="/register">Register</Link>
             <Link to="/login">Login</Link>
-            <Link to="/">Home</Link>
-            <Link to="/profile">Profile</Link>
-            <Link to="/dashboard">Dashboard</Link>
-            <Link to="/about">About</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/services">Services</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/checkout">Checkout</Link>
-            <Link to="/orders">Orders</Link>
-            <Link to="/wishlist">Wishlist</Link>
         </div>
     )
 }
