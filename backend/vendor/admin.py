51b615cc91da566e17468952a4cf24c939384d8a
rinosetiyo <rino.setiyo96@gmail.com>
2025-04-14 23:30:47 +0700

    feat: register all vendor-related models in the admin site

--- backend/vendor/admin.py
+++ backend/vendor/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from vendor.models import Vendor, VendorProfile, VendorPayout, VendorNotification, VendorWallet, VendorReview, VendorDiscount, VendorProduct, VendorProductImage, VendorProductVariant, VendorProductVariantImage
 # Register your models here.
+
+admin.site.register(Vendor)
+admin.site.register(VendorProfile)
+admin.site.register(VendorPayout)
+admin.site.register(VendorNotification)
+admin.site.register(VendorWallet)
+admin.site.register(VendorReview)
+admin.site.register(VendorDiscount)
+admin.site.register(VendorProduct)
+admin.site.register(VendorProductImage)
+admin.site.register(VendorProductVariant)
+admin.site.register(VendorProductVariantImage)  
